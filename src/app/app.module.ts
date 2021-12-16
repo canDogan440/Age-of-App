5b6b36455f8480a82c552ab7c6370b9efa42f6da
canDogan440 <candogan477@gmail.com>
2021-12-16 11:33:46 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { HttpClientModule } from '@angular/common/http';
-
-
 import { FormsModule } from '@angular/forms';
 import { UnitsListComponent } from './pages/units-list/units-list.component';
 import { UnitDetailsComponent } from './pages/units-list/unit-details/unit-details.component';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-
 
 @NgModule({
   declarations: [AppComponent, UnitsListComponent, UnitDetailsComponent],
@@ -23,16 +15,8 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatSelectModule,
-    MatFormFieldModule,
     HttpClientModule,
     FormsModule,
-
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production,
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
